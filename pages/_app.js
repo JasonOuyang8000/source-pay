39955dc0f537c207dc0da86b57bf8f3b71d744f8
Jason  Ouyang <jasonouyang@Jasons-MBP.attlocal.net>
2022-12-06 15:37:42 -0800

    setup font and add next ui library

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Varela_Round } from "@next/font/google";
+import { NextUIProvider } from "@nextui-org/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// If loading a variable font, you don't need to specify the font weight
+const varelaRound = Varela_Round({
+  weight: "400",
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider>
+      <main className={varelaRound.className}>
+        <Component {...pageProps} />
+      </main>
+    </NextUIProvider>
+  );
 }
-
-export default MyApp
